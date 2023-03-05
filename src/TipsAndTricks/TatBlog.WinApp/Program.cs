903554eb7a62ca77447a2d9378676d14106dd095
Loc Duong <myloc442@gmail.com>
2023-03-05 11:26:19 +0700

    set up project asp.net core & add library client-side

--- src/TipsAndTricks/TatBlog.WinApp/Program.cs
+++ src/TipsAndTricks/TatBlog.WinApp/Program.cs
@@ -71,24 +71,24 @@ var context = new BlogDbContext();
 //    Console.WriteLine("{0,-5}{1,-50}{2,20}", item.Id, item.Name, item.PostCount);
 //}
 
-IBlogRepository blogRepo = new BlogRepository(context);
-var pagingParams = new PagingParams
-{
-    PageNumber = 1,
-    PageSize = 5,
-    SortColumn = "Name",
-    SortOrder = "DESC"
-};
+//IBlogRepository blogRepo = new BlogRepository(context);
+//var pagingParams = new PagingParams
+//{
+//    PageNumber = 1,
+//    PageSize = 5,
+//    SortColumn = "Name",
+//    SortOrder = "DESC"
+//};
 
-var tagsList = await blogRepo.GetPagedTagsAsync(pagingParams);
+//var tagsList = await blogRepo.GetPagedTagsAsync(pagingParams);
 
-Console.WriteLine("{0, -5}{1,-50}{2,10}", "ID", "Name", "Count");
+//Console.WriteLine("{0, -5}{1,-50}{2,10}", "ID", "Name", "Count");
 
-foreach (var item in tagsList)
-{
-    Console.WriteLine("{0, -5}{1,-50}{2,10}",
-        item.Id, item.Name, item.PostCount);
-}
+//foreach (var item in tagsList)
+//{
+//    Console.WriteLine("{0, -5}{1,-50}{2,10}",
+//        item.Id, item.Name, item.PostCount);
+//}
 
 // Tìm một thẻ (Tag) theo tên định danh (slug)
 //string slug = "visual-studio";
