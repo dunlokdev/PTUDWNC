a35c303be7748e3cb309de9790aeffb802bcaa88
Loc Duong <myloc442@gmail.com>
2023-03-06 21:03:53 +0700

    create class PostQuery

---
+++ src/TipsAndTricks/TatBlog.Core/DTO/PostQuery.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TatBlog.Core.DTO
+{
+    public class PostQuery
+    {
+        public int AuthorId { get; set; }
+        public int CategoryId { get; set; }
+        public string SlugCategory { get; set; }
+        public int MonthCreated { get; set; }
+        public int YearCreated { get; set; }
+        public string Tag { get; set; }
+    }
+}
