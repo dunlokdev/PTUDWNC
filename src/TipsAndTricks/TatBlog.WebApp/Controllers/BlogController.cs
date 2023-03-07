6a65a6dcfeae5996ee6aef29e40083473896f0e6
Loc Duong <myloc442@gmail.com>
2023-03-07 13:56:14 +0700

    create controller & action

---
+++ src/TipsAndTricks/TatBlog.WebApp/Controllers/BlogController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TatBlog.WebApp.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.CurrentTime = DateTime.Now.ToString("HH:mm:ss");
+            return View();
+        }
+    }
+}
