a3f677074cf79c1595874e7bd864f0594caaf212
Loc Duong <myloc442@gmail.com>
2023-03-08 10:59:09 +0700

    done homework

--- src/TipsAndTricks/TatBlog.WebApp/Program.cs
+++ src/TipsAndTricks/TatBlog.WebApp/Program.cs
@@ -2,72 +2,19 @@
 using TatBlog.Data.Contexts;
 using TatBlog.Data.Seeders;
 using TatBlog.Services.Blogs;
+using TatBlog.WebApp.Extentions;
 
 var builder = WebApplication.CreateBuilder(args);
-{ 
-    // Thêm các dịch vụ được yêu cầu bởi MVC Framework
-    builder.Services.AddControllersWithViews();
-
-    builder.Services.AddDbContext<BlogDbContext>(
-        options => options.UseSqlServer(
-            builder.Configuration.GetConnectionString("DefaultConnection"))
-        );
-
-    builder.Services.AddScoped<IBlogRepository, BlogRepository>();
-    builder.Services.AddScoped<IDataSeeder, DataSeeder>();
+{
+    builder.ConfigureMvc()
+        .ConfigureServices();
 }
 
 var app = builder.Build();
 {
-    // Cấu hình HTTP Request pipeline
-
-    // Thêm middleware để hiển thị thông báo lỗi
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseDeveloperExceptionPage();
-    }
-    else
-    {
-        app.UseExceptionHandler("/Blog/Error");
-
-        // Thêm middleware cho việc áp dụng HSTS (Thêm header Strict-Transport-Security vào HTTP Response
-        app.UseHsts();
-    }
-
-    // Thêm middleware để chuyển hướng HTTP sang HTTPS
-    app.UseHttpsRedirection();
-
-    // Thêm midleware phục vụ các yêu cầu liên quan tới các tập tin tĩnh
-    app.UseStaticFiles();
-    
-    app.UseRouting();
-
-    // Định nghĩa route template, route constraint cho các endpoints kết hợp với các actions trong các controller
-    app.MapControllerRoute(
-        name: "posts-by-category",
-        pattern: "blog/category/{slug}",
-        defaults: new { controller = "Blog", action = "Category" });
-
-    app.MapControllerRoute(
-        name: "posts-by-tag",
-        pattern: "blog/tag/{slug}",
-        defaults: new { controller = "Blog", action = "Tag" });
-
-    app.MapControllerRoute(
-        name: "single-post",
-        pattern: "blog/post/{year:int}/{month:int}/{day:int}/{slug}",
-        defaults: new { controller = "Blog", action = "Post" });
-
-    app.MapControllerRoute(
-        name: "default",
-        pattern: "{controller=Blog}/{action=Index}/{id?}");
-}
-
-// Dữ liệu mẫu trước khi Run App
-using (var scope = app.Services.CreateScope())
-{
-    var seeder = scope.ServiceProvider.GetRequiredService<IDataSeeder>();
-    seeder.Initialize();
+    app.UserRequestPipeline();
+    app.UseBlogRoutes();
+    app.UseDataSeeder();
 }
 
 app.Run();
