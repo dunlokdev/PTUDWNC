a735a258ca5a8791490e1bbf2a2467f647802905
Loc Duong <myloc442@gmail.com>
2023-03-08 10:36:36 +0700

    feature search

--- src/TipsAndTricks/TatBlog.WebApp/Controllers/BlogController.cs
+++ src/TipsAndTricks/TatBlog.WebApp/Controllers/BlogController.cs
@@ -13,15 +13,17 @@ namespace TatBlog.WebApp.Controllers
             _blogRepository = blogRepository;
         }
         public async Task<IActionResult> Index(
+            [FromQuery(Name = "k")] string keyword = null,
             [FromQuery(Name = "p")] int pageNumber = 1,
             [FromQuery(Name = "ps")] int pageSize = 3)
         {
             var postQuery = new PostQuery()
             {
                 PublishedOnly = true,
+                KeyWord = keyword,
             };
 
-            var postsList = await _blogRepository.GetPagedPostsAsync( postQuery, pageNumber, pageSize);
+            var postsList = await _blogRepository.GetPagedPostsAsync(postQuery, pageNumber, pageSize);
 
             ViewBag.PostQuery = postQuery;
 
