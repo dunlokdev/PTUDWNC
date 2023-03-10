1e11a8919ea4778a784d4a4e61b4eafe84599e76
Loc Duong <myloc442@gmail.com>
2023-03-10 22:57:30 +0700

    Done: FeaturePosts

---
+++ src/TipsAndTricks/TatBlog.WebApp/Components/OutstandPostWidget.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TatBlog.Services.Blogs;
+
+namespace TatBlog.WebApp.Components
+{
+    public class OutstandPostWidget: ViewComponent
+    {
+        private readonly IBlogRepository _blogRepository;
+
+        public OutstandPostWidget(IBlogRepository blogRepository)
+        {
+            _blogRepository = blogRepository;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var posts = await _blogRepository.GetPopularArticleAsync(3);
+
+            return View(posts);
+        }
+
+    }
+}
