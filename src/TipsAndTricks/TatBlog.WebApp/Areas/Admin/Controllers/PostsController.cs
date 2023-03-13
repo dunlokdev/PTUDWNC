140210c3612d1c46ba91905a965422cb40e3a848
Loc Duong <myloc442@gmail.com>
2023-03-13 23:47:00 +0700

    Using mapper :D This is very consice !

--- src/TipsAndTricks/TatBlog.WebApp/Areas/Admin/Controllers/PostsController.cs
+++ src/TipsAndTricks/TatBlog.WebApp/Areas/Admin/Controllers/PostsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MapsterMapper;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using TatBlog.Core.DTO;
 using TatBlog.Services.Blogs;
@@ -9,21 +10,26 @@ namespace TatBlog.WebApp.Areas.Admin.Controllers
     public class PostsController : Controller
     {
         private readonly IBlogRepository _blogRepository;
+        private readonly IMapper _mapper;
 
-        public PostsController(IBlogRepository blogRepository)
+        public PostsController(IBlogRepository blogRepository, IMapper mapper)
         {
             _blogRepository = blogRepository;
+            _mapper = mapper;
         }
         public async Task<IActionResult> Index(PostFilterModel model)
         {
-            var postQuery = new PostQuery()
-            {
-                KeyWord = model.Keyword,
-                CategoryId = model.CategoryId,
-                AuthorId = model.AuthorId,
-                Year = model.Year,
-                Month = model.Month
-            };
+            //var postQuery = new PostQuery()
+            //{
+            //    KeyWord = model.Keyword,
+            //    CategoryId = model.CategoryId,
+            //    AuthorId = model.AuthorId,
+            //    Year = model.Year,
+            //    Month = model.Month
+            //};
+
+            // using library map :D --> fast, concise: gọn
+            var postQuery = _mapper.Map<PostQuery>(model);
 
             ViewBag.PostsList = await _blogRepository.GetPagedPostsAsync(postQuery, 1, 10);
 
