8f306ecea14a85b2edf3545a2ff5a5f91646729f
dunlok <myloc442@gmail.com>
2023-03-27 22:14:50 +0700

    Define endpoint GetAuthors

---
+++ src/TipsAndTricks/TatBlog.WebApi/Endpoints/AuthorEndpoints.cs
@@ -0,0 +1,30 @@
+﻿using TatBlog.Core.Collections;
+using TatBlog.Core.DTO;
+using TatBlog.Services.Blogs;
+using TatBlog.WebApi.Models;
+
+namespace TatBlog.WebApi.Endpoints
+{
+    public static class AuthorEndpoints
+    {
+        public static WebApplication MapAuthorEndpoints(this WebApplication app)
+        {
+            var routeGroupBuilder = app.MapGroup("/api/authors");
+
+            routeGroupBuilder.MapGet("/", GetAuthors)
+                .WithName("GetAuthors")
+                .Produces<PaginationResult<AuthorItem>>();
+
+            return app;
+        }
+
+        private static async Task<IResult> GetAuthors([AsParameters] AuthorFilterModel model, IAuthorRepository authorRepository)
+        {
+            var authorList = await authorRepository.GetPagedAuthorsAsync(model, model.Name);
+
+            var paginationResult = new PaginationResult<AuthorItem>(authorList);
+
+            return Results.Ok(paginationResult);
+        }
+    }
+}
--- src/TipsAndTricks/TatBlog.WebApi/Program.cs
+++ src/TipsAndTricks/TatBlog.WebApi/Program.cs
@@ -1,3 +1,4 @@
+using TatBlog.WebApi.Endpoints;
 using TatBlog.WebApi.Extensions;
 using TatBlog.WebApi.Mapsters;
 using TatBlog.WebApi.Validations;
@@ -17,6 +18,9 @@ var app = builder.Build();
 {
     // Configure the HTTP request pipeline
     app.SetupRequestPipeline();
+
+    // Configure API Endpoints
+    app.MapAuthorEndpoints();
 }
 
 app.Run();
